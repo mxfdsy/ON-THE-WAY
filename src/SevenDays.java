ee94ca9d104c7c72d5b834effcc5208bb06680bd
平凡的世界 <963559210@qq.com>
2018-03-21 23:02:28 +0800

    明天再来

---
+++ src/SevenDays.java
@@ -0,0 +1,17 @@
+import java.util.Calendar;
+import java.util.Date;
+
+import static java.util.Calendar.YEAR;
+
+/**
+ * 需求 表示最近七天（包括今天）
+ * Created by 平凡的世界 on 2018/3/21.
+ * @author 没想法的岁月
+ */
+public class SevenDays {
+    public static void main(String[] args) {
+        Calendar instance = Calendar.getInstance();
+
+
+    }
+}
