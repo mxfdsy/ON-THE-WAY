d1ce76be745d4f1ff742bbf59e8e2e98b75d6b03
平凡的世界 <963559210@qq.com>
2018-03-23 23:23:26 +0800

    1、用到了 格式转化 date--String
    2、时间的增减和设置对应的时间值
    
    3、日历类

--- src/SevenDays.java
+++ src/SevenDays.java
@@ -30,6 +30,7 @@ public class SevenDays {
             c.set(Calendar.HOUR_OF_DAY,0);
             c.set(Calendar.MINUTE,0);
             c.set(Calendar.SECOND,0);
+            
         }
     }
 }
