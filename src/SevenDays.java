5b839d20a2d717e99dab7a5b9ecc3b26702d16f4
平凡的世界 <963559210@qq.com>
2018-03-23 23:22:20 +0800

    1、用到了 格式转化 date--String
    2、时间的增减和设置对应的时间值
    
    3、日历类

--- src/SevenDays.java
+++ src/SevenDays.java
@@ -1,6 +1,7 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Currency;
 import java.util.Date;
 
 import static java.util.Calendar.YEAR;
@@ -13,6 +14,7 @@ import static java.util.Calendar.YEAR;
 public class SevenDays {
     public static void main(String[] args) {
         seachTime(7);
+
     }
 
     private static void seachTime(int recent) {
@@ -20,10 +22,14 @@ public class SevenDays {
         Calendar c = Calendar.getInstance();
         DateFormat dateTimeInstance = DateFormat.getDateTimeInstance();
 
+
         for (int i = 0; i < recent ; i++) {
             String format = dateTimeInstance.format(c.getTime());
             System.out.println(format);
             c.add(Calendar.DAY_OF_MONTH,-1);
+            c.set(Calendar.HOUR_OF_DAY,0);
+            c.set(Calendar.MINUTE,0);
+            c.set(Calendar.SECOND,0);
         }
     }
 }
